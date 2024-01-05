afc18e4378d4071fbd7eb5116b93f27158b35eaf
Alfredo J. Romero M <alfredorm2353@gmail.com>
2024-01-05 15:56:30 -0500

    Update runThemeCheck.js
    Change the shopify theme command

--- runThemeCheck.js
+++ runThemeCheck.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
-
-exec('shopify theme check', (error, stdout, stderr) => {
+const cwd = process.cwd()
+exec(`${cwd}/node_modules/.bin/shopify theme check`, (error, stdout, stderr) => {
     if (error) {
         console.error(`error: ${error.message}`);
         return;
