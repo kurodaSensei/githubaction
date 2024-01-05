bd6ada546febf295b3e0aeb949cf2dd588218e5b
Alfredo J. Romero M <alfredorm2353@gmail.com>
2024-01-05 16:07:27 -0500

    Update runThemeCheck.js
    Fix command error

--- runThemeCheck.js
+++ runThemeCheck.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exec(`theme check -o json .`, (error, stdout, stderr) => {
+exec(`theme-check -o json .`, (error, stdout, stderr) => {
     if (error) {
         console.error(`error: ${error.message}`);
         return;
